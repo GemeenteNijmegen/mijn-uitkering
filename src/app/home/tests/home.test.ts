5ff865ede650a3ffed8ebe88f91a7a77f01de3d3
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-01-20 17:23:34 +0100

    chore: lint

--- src/app/home/tests/home.test.ts
+++ src/app/home/tests/home.test.ts
@@ -4,37 +4,37 @@ import * as lambda from '../index';
 
 const ddbMock = mockClient(DynamoDBClient);
 beforeAll(() => {
-    global.console.log = jest.fn();
-    // Set env variables
-    process.env.SESSION_TABLE = 'mijnuitkering-sessions';
-    process.env.AUTH_URL_BASE = 'https://authenticatie-accp.nijmegen.nl';
-    process.env.APPLICATION_URL_BASE = 'https://testing.example.com/';
-    process.env.CLIENT_SECRET_ARN = '123';
-    process.env.OIDC_CLIENT_ID = '1234';
-    process.env.OIDC_SCOPE = 'openid';
+  global.console.log = jest.fn();
+  // Set env variables
+  process.env.SESSION_TABLE = 'mijnuitkering-sessions';
+  process.env.AUTH_URL_BASE = 'https://authenticatie-accp.nijmegen.nl';
+  process.env.APPLICATION_URL_BASE = 'https://testing.example.com/';
+  process.env.CLIENT_SECRET_ARN = '123';
+  process.env.OIDC_CLIENT_ID = '1234';
+  process.env.OIDC_SCOPE = 'openid';
 });
 
 beforeEach(() => {
-    ddbMock.mockReset();
-    const getItemOutput: Partial<GetItemCommandOutput> = {
+  ddbMock.mockReset();
+  const getItemOutput: Partial<GetItemCommandOutput> = {
     Item: {
-        loggedin: {
+      loggedin: {
         BOOL: true,
-        },
-        state: {
+      },
+      state: {
         S: '12345',
-        },
+      },
     },
-    };
-    ddbMock.mockImplementation(() => getItemOutput);
-})
+  };
+  ddbMock.mockImplementation(() => getItemOutput);
+});
 
 test('Returns 200', async () => {
-    const result = await lambda.handler({ cookies: [`session=12345`] }, {});
-    expect(result.statusCode).toBe(200);
+  const result = await lambda.handler({ cookies: ['session=12345'] }, {});
+  expect(result.statusCode).toBe(200);
 });
 
 test('Shows overview page', async () => {
-    const result = await lambda.handler({ cookies: [`session=12345`] }, {});
-    expect(result.body).toMatch(`Mijn Uitkering`);
+  const result = await lambda.handler({ cookies: ['session=12345'] }, {});
+  expect(result.body).toMatch('Mijn Uitkering');
 });
