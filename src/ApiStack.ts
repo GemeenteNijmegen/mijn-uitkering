badd4e4205e51cb4ceb9f53729fca226b6e4b7d3
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-02-17 10:03:16 +0100

    Extract host from api url

--- src/ApiStack.ts
+++ src/ApiStack.ts
@@ -22,7 +22,8 @@ export class ApiStack extends Stack {
     const api = new apigatewayv2.HttpApi(this, 'mijnuitkering-api', {
       description: 'Mijn Uitkering webapplicatie',
     });
-    this.apiGatewayDomain = api.url;
+    const apiUrl = new URL(api.url);
+    this.apiGatewayDomain = apiUrl.hostname;
     const loginFunction = new ApiFunction(this, 'login-function', {
       description: 'Login-pagina voor de Mijn Uitkering-applicatie.',
       codePath: 'app/login',
