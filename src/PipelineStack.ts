fb3746073722f04a7d582a98ae96152466e9d748
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-03-21 15:02:23 +0100

    Update dependencies

--- src/PipelineStack.ts
+++ src/PipelineStack.ts
@@ -19,7 +19,6 @@ export class PipelineStack extends Stack {
     this.branchName = props.branchName;
     const pipeline = this.pipeline();
     pipeline.addStage(new ParameterStage(this, 'mijn-nijmegen-parameters', { env: props.deployToEnvironment }));
-    pipeline.addStage(new ApiStage(this, 'mijn-api', { env: props.deployToEnvironment, branch: this.branchName }));
     pipeline.addStage(new UitkeringsApiStage(this, 'mijn-uitkering-api', { env: props.deployToEnvironment, branch: this.branchName }));
   }
 
